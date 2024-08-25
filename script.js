6bcf3901a21dee9451da02f8743fe986e2f4aee0
sunilaadhe <sunilaadhe62@gmail.com>
2024-08-25 17:42:20 +0530

    chenhe big

--- script.js
+++ script.js
@@ -1,4 +1,31 @@
-let button=document.querySelector("button");
-button.addEventListener("click",()=>{
-  button.innerText="js";
-});
+let button=document.querySelectorAll("button");
+let input=document.getElementById("#input");
+      
+   let String="";
+   let arr=array.form(button);
+   arr.forEach(button=>{
+    button.addEventListener("click",(e)=>{
+      if(e.target,innerHTML=="=") {
+        String= eval(string);
+        input.value=string;
+      } 
+       else if(e. target.innerHTML=="AC"){
+
+       }
+
+       else if(e.target.innerHTML=="DEL"){
+        srting=string.substring(0,string.length-1);
+        input.value=srting;
+       }
+
+       else{
+      string+=e.target.innerHTML;
+      input.value=string;
+       }
+    })
+    
+   })
+
+   
+
+
