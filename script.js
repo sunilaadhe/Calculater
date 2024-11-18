ea276ea3807dd328d48b420884bd83faa940b5d8
sunilaadhe <sunilaadhe62@gmail.com>
2024-11-18 21:27:32 +0530

    done

--- script.js
+++ script.js
@@ -1,30 +1,30 @@
-let button=document.querySelectorAll("button");
-let input=document.getElementById("#input");
+let button = document.querySelectorAll("button");
+let input = document.getElementById("input");
       
-   let String="";
-   let arr=array.form(button);
+   let String ='';
+   let arr = array.from(buttons);
    arr.forEach(button=>{
     button.addEventListener("click",(e)=>{
       if(e.target,innerHTML=="=") {
-        String= eval(string);
-        input.value=string;
+        String = eval(string);
+        input.value = string;
       } 
-       else if(e. target.innerHTML=="AC"){
+       else if(e. target.innerHTML==="AC"){
 
        }
 
-       else if(e.target.innerHTML=="DEL"){
-        srting=string.substring(0,string.length-1);
-        input.value=srting;
+       else if(e.target.innerHTML==="DEL"){
+        srting = string.substring(0,string.length-1);
+        input.value = srting;
        }
 
        else{
-      string+=e.target.innerHTML;
-      input.value=string;
+      string += e.target.innerHTML;
+      input.value= string;
        }
-    })
+    });
     
-   })
+   });
 
    
 
